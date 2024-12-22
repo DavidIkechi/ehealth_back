df282f0c5b09fb247c2a6d829617ed390d3d8562
DavidIkechi <daakwuru@gmail.com>
2024-12-22 16:43:22 +0000

    .

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 # Install dependencies
-pip install -r requirements.txt
+python3 -m ensurepip --upgrade
+python3 -m pip install --upgrade pip
+
+# Install project dependencies
+python3 -m pip install -r requirements.txt
